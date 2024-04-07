f1e6851fb6fabc36a363830d52ffbdab67dcc84a
N-Hiro0123 <n.hiroki0909@gmail.com>
2024-04-07 21:45:06 +0900

    /chatrawdatas "POST","GET"、/chatposts "POST", "GET"を作成。APIの詳細はエクセルを参照

--- backend/app.py
+++ backend/app.py
@@ -2,10 +2,13 @@ from flask import Flask, request
 from flask import jsonify
 import json
 from flask_cors import CORS
+import pandas as pd
 
 from db_control import crud, mymodels
 
 import requests
+
+from datetime import datetime
 
 # Azure Database for MySQL
 # REST APIでありCRUDを持っている
@@ -18,8 +21,9 @@ def index():
     return "<p>Flask top page!</p>"
 
 
+# チャットの生データを挿入する
 @app.route("/chatrawdatas", methods=["POST"])
-def create_customer():
+def create_chatrawdata():
     values = request.get_json()
     # values = {
     #     "parent_user_id": XX,
@@ -30,3 +34,75 @@ def create_customer():
 
     result = crud.chatrawinsert(mymodels.ChatRawDatas, values)
     return result, 200
+
+
+# サーバー時刻を返す
+@app.route("/chatposts", methods=["GET"])
+def get_chatpost():
+
+    current_utc_datetime = datetime.utcnow().strftime(('%Y-%m-%d %H:%M:%S.%f'))
+    result = {
+        "created_at": current_utc_datetime,
+    }
+
+    return result, 200
+
+
+# 投稿管理テーブルへ挿入する
+@app.route("/chatposts", methods=["POST"])
+def create_chatpost():
+    values = request.get_json()
+    # values = {
+    #     "parent_user_id": 1,
+    #     "child_user_id": 2,
+    #     "recording_start_datetime": ('%Y-%m-%d %H:%M:%S.%f'),
+    #     "recording_end_datetime": ('%Y-%m-%d %H:%M:%S.%f'),
+    # }
+    print(values)
+
+    result = crud.chatpostinsert(mymodels.ChatPosts, values)
+    return result, 200
+
+
+# ChatPostsから会話の開始と終了を読み込んで、ChatRawDatasの内容を整理してjson形式で返す
+@app.route("/chatrawdatas", methods=["GET"])
+def get_chatrawdata():
+
+    values = request.get_json()
+    # values = {
+    #     "parent_user_id": XX,
+    #     "child_user_id": YY,
+    # }
+    print(values)
+
+    # ChatPostsから録音開始時間と録音終了時間をすべて取り込む
+    chatposts_df = crud.read_chatposts(mymodels.ChatPosts, values)
+
+    result_dict_list = []  # 　整理後の履歴を
+
+    for i, chatpost_df in chatposts_df.iterrows():
+        chatrawdatas_df = crud.read_chatrawdatas(mymodels.ChatRawDatas, chatpost_df)
+
+        contents = ""  # 初期化
+        for ii, chatrawdata_df in chatrawdatas_df.iterrows():
+            contents = contents + chatrawdata_df["content"]
+
+        parent_user_id = chatpost_df["parent_user_id"]
+        child_user_id = chatpost_df["child_user_id"]
+        chatpost_created_at_str = chatpost_df["recording_end_datetime"].strftime('%Y-%m-%d %H:%M:%S.%f')
+
+        result_dict_list.append(
+            {
+                "chatpost_created_at": chatpost_created_at_str,
+                # "parent_user_id": parent_user_id,
+                # "child_user_id": child_user_id,
+                "content": contents,
+            }
+        )
+
+    print(result_dict_list)
+    result_json = json.dumps(result_dict_list, ensure_ascii=False)
+
+    # result = chatrawdata_df.to_json(orient="records", force_ascii=False)
+
+    return result_json, 200
--- backend/db_control/crud.py
+++ backend/db_control/crud.py
@@ -9,12 +9,13 @@ import sqlalchemy
 from sqlalchemy.orm import sessionmaker
 import json
 import pandas as pd
+from datetime import datetime, date
 
 from db_control.connect import engine
 from db_control.mymodels import ChatRawDatas
 
 
-def chatrawinsert(mymodel):
+def chatrawinsert(mymodel, values):
     # session構築
     Session = sessionmaker(bind=engine)
     session = Session()
@@ -30,6 +31,99 @@ def chatrawinsert(mymodel):
         session.rollback()
         return "error"
 
-    # セッションを閉じる
-    session.close()
+    finally:
+        # セッションを閉じる
+        session.close()
+
     return "inserted"
+
+
+def chatpostinsert(mymodel, values):
+    # session構築
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # datetime文字列をdatetimeオブジェクトへ変換
+    recording_start_time_str = values.get("recording_start_datetime")
+    recording_end_time_str = values.get("recording_end_datetime")
+    values["recording_start_datetime"] = datetime.strptime(recording_start_time_str, '%Y-%m-%d %H:%M:%S.%f')
+    values["recording_end_datetime"] = datetime.strptime(recording_end_time_str, '%Y-%m-%d %H:%M:%S.%f')
+
+    query = insert(mymodel).values(values)
+
+    try:
+        # トランザクションを開始
+        with session.begin():
+            # データの挿入
+            result = session.execute(query)
+    except sqlalchemy.exc.IntegrityError:
+        print("挿入に失敗しました")
+        session.rollback()
+        return "error"
+
+    finally:
+        # セッションを閉じる
+        session.close()
+
+    return "inserted"
+
+
+def read_chatposts(mymodel, values):
+    # session構築
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    parent_user_id = values.get("parent_user_id")
+    child_user_id = values.get("child_user_id")
+
+    # paret_user_id, child_user_idが一致するものを抽出
+    query = session.query(mymodel).filter(mymodel.parent_user_id == parent_user_id, mymodel.child_user_id == child_user_id)
+
+    try:
+        # query結果をpdにする場合はread_sqlでよいみたい
+        df = pd.read_sql(query.statement, session.bind)
+
+    except sqlalchemy.exc.IntegrityError as e:
+        print("ChatPostsからの読み込みに失敗しました", e)
+        session.rollback()
+        return "error"
+
+    finally:
+        # セッションを閉じる
+        session.close()
+
+    return df
+
+
+def read_chatrawdatas(mymodel, value):
+    # session構築
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    parent_user_id = value.get("parent_user_id")
+    child_user_id = value.get("child_user_id")
+    recording_start_datetime = value.get("recording_start_datetime")  # datetime
+    recording_end_datetime = value.get("recording_end_datetime")  # datetime
+
+    # paret_user_id, child_user_idが一致するものを抽出
+    query = session.query(mymodel).filter(
+        mymodel.parent_user_id == parent_user_id,
+        mymodel.child_user_id == child_user_id,
+        mymodel.created_at >= recording_start_datetime,
+        mymodel.created_at <= recording_end_datetime,
+    )
+
+    try:
+        # query結果をpdにする場合はread_sqlでよいみたい
+        df = pd.read_sql(query.statement, session.bind)
+
+    except sqlalchemy.exc.IntegrityError as e:
+        print("ChatPostsからの読み込みに失敗しました", e)
+        session.rollback()
+        return "error"
+
+    finally:
+        # セッションを閉じる
+        session.close()
+
+    return df
